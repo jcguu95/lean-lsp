ff307a20506821c01f1b24c926fcc97ed8159143
jcguu95 <jcguu95@gmail.com>
2026-02-09 16:36:17 -0700

    Remove test-project.

--- bin/test-project/ExampleProject.lean
+++
@@ -1,4 +0,0 @@
-import Mathlib.Data.Nat.Prime.Basic
-
--- Let's define a proposition using a definition from mathlib.
-def is_seventeen_prime : Prop := Nat.Prime 17
--- bin/test-project/Main.lean
+++
@@ -1,4 +0,0 @@
-import TestProject
-
-def main : IO Unit :=
-  IO.println s!"Hello, {hello}!"
